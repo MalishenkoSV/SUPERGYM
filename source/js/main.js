74b7f42262af4668e2246c73a5fc69fafb7c92da
Svetlana <lisalana15@gmail.com>
2020-06-10 12:03:22 +0300

    Футер мобильный праdbn  и слайдер отзывов

--- source/js/main.js
+++ source/js/main.js
@@ -1,21 +1,27 @@
 'use strict';
-var slider = document.querySelector('.slider');
-var activeTrainers = slider.querySelector('.trainers__list--one');
-var activeTrainersTwo = slider.querySelector('.trainers__list--two');
-// var phone = popup.querySelector('#popup-tel');
-// var question = popup.querySelector('#popup-question');
-var startBtn = slider.querySelector('.slider__item--start');
-var finishBtn = slider.querySelector('.slider__item--finish');
-
-
 // для табов
-var togglesList = document.querySelector('.controls');
-var toggles = togglesList.querySelectorAll('.controls__btn');
-var abonements = document.querySelectorAll('.abonements');
-var abonementsList = abonements.querySelectorAll('.abonements__list');
-var firstAbonementsList = abonements.querySelector('.abonements__list--first');
-var secondAbonementsList = abonements.querySelector('.abonements__list--second');
-var thirdAbonementsList = abonements.querySelector('.abonements__list--third');
+var controls = document.querySelector('.controls');
+var togglesList = controls.querySelectorAll('.controls__btn');
+var abonementsLists = document.querySelectorAll('.abonements__list');
+var firstAbonementsList = document.querySelector('.abonements__list--first');
+var secondAbonementsList = document.querySelector('.abonements__list--second');
+var thirdAbonementsList = document.querySelector('.abonements__list--third');
+
+// Скролл страницы
+var btnScreen = document.querySelector('.main-screen__btn');
+
+if (btnScreen) {
+  btnScreen.addEventListener('click', function (evt) {
+    evt.preventDefault();
+
+    var blockSubscription = btnScreen.getAttribute('href');
+
+    document.querySelector(blockSubscription).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+}
 
 var togglesMap = {
   first: 'controls__btn--first',
@@ -23,42 +29,24 @@ var togglesMap = {
   third: 'controls__btn--third',
 };
 
-// для слайдера тренеров
-var trainersNode = document.querySelector('#trainers');
-var trainersPrevButton = document.querySelector('#trainers-prev');
-var trainersNextButton = document.querySelector('#trainers-next');
-var trainers = Array.prototype.slice.call(document.querySelectorAll('.trainers__list-item'));
-
-var clientWidthMap = {
-  desktop: 1200,
-  tablet: 1199,
-  mobile: 767,
-};
-
-var trainersStepMap = {
-  desktop: 4,
-  tablet: 2,
-  mobile: 1,
-};
-
 // переключение табов в блоке Абонементы
 
-togglesList.addEventListener('click', function (evt) {
-  if (!evt.target.closest('controls__btn').classList.contains('controls__btn--active')) {
-    for (var i = 0; i < toggles.length; i++) {
-      if (toggles[i].classList.contains('controls__btn--active')) {
-        toggles[i].classList.remove('controls__btn--active');
+controls.addEventListener('click', function (evt) {
+  if (!evt.target.closest('button').classList.contains('controls__btn--active')) {
+    togglesList.forEach(function (toggle) {
+      if (toggle.classList.contains('controls__btn--active')) {
+        toggle.classList.remove('controls__btn--active');
       }
-    }
+    });
 
     var toggleButton = evt.target.closest('button');
     toggleButton.classList.add('controls__btn--active');
 
-    for (var j = 0; j < abonementsList.length; j++) {
-      if (abonementsList[j].classList.contains('abonemens__list--shown')) {
-        abonementsList[j].classList.remove('abonemens__list--shown');
+    abonementsLists.forEach(function (item) {
+      if (item.classList.contains('abonemens__list--shown')) {
+        item.classList.remove('abonemens__list--shown');
       }
-    }
+    });
 
     if (toggleButton.classList.contains(togglesMap.first)) {
       firstAbonementsList.classList.add('abonemens__list--shown');
@@ -71,19 +59,115 @@ togglesList.addEventListener('click', function (evt) {
 });
 
 
-activeTrainers.classList.add('trainers__list--active');
-finishBtn.addEventListener('click', function () {
-  if (activeTrainers.classList.contains('trainers__list--active')) {
-    activeTrainers.classList.remove('trainers__list--hidden');
-    activeTrainers.classList.add('trainers__list--visual');
+// Тренеры
+var slider = document.querySelector('.slider');
+var activeTrainer = slider.querySelector('.trainers__list--one');
+var activeTrainerTwo = slider.querySelector('.trainers__list--two');
+var startBtn = slider.querySelector('.trainers__slider-btn-left');
+var finishBtn = slider.querySelector('.trainers__slider-btn-right');
+var trainersBlock = document.querySelector('.trainers');
+var trainersList = document.querySelectorAll('.trainers__list-item');
 
-  } else {
-    activeTrainers.classList.add('trainers__list--visual');
-    activeTrainersTwo.classList.remove('trainers__list--active');
+var clientWidthMap = {
+  desktop: 1200,
+  tablet: 1199,
+  mobile: 767,
+};
+
+var trainersStep = {
+  tablet: 2,
+  mobile: 1,
+};
+
+
+finishBtn.addEventListener('click', function () {
+  if (activeTrainer.classList.contains('trainers__list--active')) {
+    activeTrainer.classList.remove('trainers__list--hidden');
+    activeTrainerTwo.classList.add('trainers__list--hidden');
+    activeTrainer.classList.remove('trainers__list--active');
+    activeTrainer.classList.add('trainers__list--active');
   }
+  // } else {
+  //   activeTrainers.classList.remove('trainers__list--visual');
+  //   activeTrainers.classList.add('trainers__list--hidden');
+  //   activeTrainers.classList.remove('trainers__list--active');
+  //   activeTrainersTwo.classList.add('trainers__list--active');
+  // }
 });
 
-// activeTrainers.classList.add('trainers__list--tablet-hidden');
-    // activeTrainers.classList.add('trainers__list--mobile-hidden');
-     // activeTrainers.classList.add('trainers__list--tablet-visual');
-    // activeTrainers.classList.add('trainers__list--mobile-visual');
+startBtn.addEventListener('click', function () {
+  if (activeTrainerTwo.classList.contains('trainers__list--active')) {
+    activeTrainerTwo.classList.remove('trainers__list--hidden');
+    activeTrainer.classList.add('trainers__list--hidden');
+    activeTrainerTwo.classList.remove('trainers__list--active');
+    activeTrainerTwo.classList.add('trainers__list--active');
+  }
+  // } else {
+  //   activeTrainersTwo.classList.remove('trainers__list--visual');
+  //   activeTrainersTwo.classList.add('trainers__list--hidden');
+  //   activeTrainers.classList.add('trainers__list--active');
+  //   activeTrainers.classList.remove('trainers__list--active');
+  // }
+});
+
+// для планшета
+
+
+
+// слайдер в блоке Отзывы
+// для слайдера отзывов
+// var reviewsNode = document.querySelector('.reviews');
+var reviewsPrevButton = document.querySelector('.reviews__slider-btn-left');
+var reviewsNextButton = document.querySelector('.reviews__slider-btn-right');
+var reviews = Array.prototype.slice.call(document.querySelectorAll('.reviews__item'));
+var reviewsStep = 1;
+
+var slideReviewsToNext = function () {
+  var currentIndex = 0;
+  for (var a = 0; a < reviews.length; a++) {
+    if (reviews[a].classList.contains('reviews__item--active')) {
+      currentIndex = a;
+      break;
+    }
+  }
+
+  reviews[currentIndex].classList.remove('reviews__item--active');
+  reviews[currentIndex].classList.add('reviews__item--hidden');
+
+  if (currentIndex < reviews.length - 1) {
+    reviews[currentIndex + reviewsStep].classList.remove('reviews__item--hidden');
+    reviews[currentIndex + reviewsStep].classList.add('reviews__item--active');
+  } else {
+    reviews[0].classList.remove('reviews__item--hidden');
+    reviews[0].classList.add('reviews__item--active');
+  }
+};
+
+var slideReviewsToPrev = function () {
+  var currentIndex = 0;
+  for (var z = 0; z < reviews.length; z++) {
+    if (reviews[z].classList.contains('reviews__item--active')) {
+      currentIndex = z;
+      break;
+    }
+  }
+
+  reviews[currentIndex].classList.remove('reviews__item--active');
+  reviews[currentIndex].classList.add('reviews__item--hidden');
+
+  if (currentIndex > 0) {
+    reviews[currentIndex - reviewsStep].classList.remove('reviews__item--hidden');
+    reviews[currentIndex - reviewsStep].classList.add('reviews__item--active');
+  } else {
+    reviews[reviews.length - 1].classList.remove('reviews__item--hidden');
+    reviews[reviews.length - 1].classList.add('reviews__item--active');
+  }
+};
+
+if (reviewsNextButton && reviews) {
+  reviewsNextButton.addEventListener('click', slideReviewsToNext);
+}
+
+if (reviewsPrevButton && reviews) {
+  reviewsPrevButton.addEventListener('click', slideReviewsToPrev);
+}
