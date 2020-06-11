972f6c3a5c3dae36e914dd6eddf20157900352d7
Svetlana <lisalana15@gmail.com>
2020-06-11 06:46:32 +0300

    Исправления по критериям

--- source/js/main.js
+++ source/js/main.js
@@ -61,8 +61,111 @@
   }
 
 
- // слайдер в блоке Отзывы
+  // Определение ширины элементов в зависимости от экрана
 
+  // var telNumber = document.querySelector('.main-form input[name="customer-phone"]');
+  var trainers = document.querySelector('.trainers');
+  var trainersList = document.querySelector('.trainers__list');
+  var trainerItemsAll = document.querySelectorAll('.trainers__item');
+  var trainerPrev = document.querySelector('.trainers__slider-btn-left');
+  var trainerNext = document.querySelector('.trainers__slider-btn-right');
+  var trainerWidth;
+  var trainerCount;
+  var trainerPosition;
+
+  var screenWidthDetection = function () {
+    if (trainers) {
+      trainersList.style.marginLeft = 0 + 'px';
+    }
+
+    if (window.matchMedia('(min-width: 320px)').matches) {
+      trainerWidth = 256; // 226px + 30px
+      trainerCount = 1;
+      trainerPosition = 0;
+    }
+
+    if (window.matchMedia('(min-width: 768px)').matches) {
+      trainerWidth = 298; // 266px + 30px
+      trainerCount = 2;
+      trainerPosition = 0;
+    }
+
+    if (window.matchMedia('(min-width: 1200px)').matches) {
+      trainerWidth = 300; // 260px + 40px
+      trainerCount = 4;
+      trainerPosition = 0;
+    }
+  };
+
+  // Если блок тренеров сущесвует на странице
+  if (trainers) {
+    // Предыдущий тренер
+    var prevTrainerItem = function () {
+      trainerPosition = Math.min(trainerPosition, 0);
+      if (trainerPosition === 0) {
+        trainerPosition = -trainerWidth * (trainerItemsAll.length - trainerCount);
+      } else {
+        trainerPosition += trainerWidth;
+      }
+      if (trainersList) {
+        trainersList.style.marginLeft = trainerPosition + 'px';
+        trainersList.style.transition = 'margin-left 0.5s';
+      }
+    };
+
+    // Следующий тренер
+    var nextTrainerItem = function () {
+      trainerPosition = Math.max(trainerPosition, -trainerWidth * (trainerItemsAll.length - trainerCount));
+      if (trainerPosition === -trainerWidth * (trainerItemsAll.length - trainerCount)) {
+        trainerPosition = 0;
+      } else {
+        trainerPosition -= trainerWidth;
+      }
+      if (trainersList) {
+        trainersList.style.marginLeft = trainerPosition + 'px';
+        trainersList.style.transition = 'margin-left 0.5s';
+      }
+    };
+
+    // Клики на контролах
+    trainerPrev.addEventListener('click', prevTrainerItem);
+    trainerNext.addEventListener('click', nextTrainerItem);
+
+    // Нажатия Enter на контролах
+    trainerPrev.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === 13) {
+        prevTrainerItem();
+      }
+    });
+
+    trainerNext.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === 13) {
+        nextTrainerItem();
+      }
+    });
+  }
+
+  // Переопределение ширины элементов при изменении экрана
+  window.addEventListener('resize', function () {
+    screenWidthDetection();
+  });
+
+  // // Маска номера телефона
+  // var telOptions = {
+  //   mask: '+{7}(000)000-00-00'
+  // };
+
+  // if (telNumber) {
+  //   var iMask;
+  //   iMask(telNumber, telOptions);
+  // }
+
+
+  // Основной код
+  screenWidthDetection();
+
+
+  // слайдер в блоке Отзывы
   // var reviewsNode = document.querySelector('.reviews');
   var reviewsPrevButton = document.querySelector('.reviews__slider-btn-left');
   var reviewsNextButton = document.querySelector('.reviews__slider-btn-right');
