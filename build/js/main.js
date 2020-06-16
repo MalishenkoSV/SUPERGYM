0e989d4dd32664fa492f570ca1285fe0c6d7f7ba
Svetlana <lisalana15@gmail.com>
2020-06-16 08:10:33 +0300

    Удалила коментарии

--- build/js/main.js
+++ build/js/main.js
@@ -260,6 +260,46 @@ if (telInputs) {
 
 'use strict';
 
+var telInputs = document.querySelectorAll('input[type="tel"]');
+
+if (telInputs) {
+  telInputs.forEach(function (input) {
+    Inputmask('+7 (999) 999-99-99').mask(input);
+  });
+}
+
+'use strict';
+
+var telInputs = document.querySelectorAll('input[type="tel"]');
+
+if (telInputs) {
+  telInputs.forEach(function (input) {
+    Inputmask('+7 (999) 999-99-99').mask(input);
+  });
+}
+
+'use strict';
+
+var telInputs = document.querySelectorAll('input[type="tel"]');
+
+if (telInputs) {
+  telInputs.forEach(function (input) {
+    Inputmask('+7 (999) 999-99-99').mask(input);
+  });
+}
+
+'use strict';
+
+var telInputs = document.querySelectorAll('input[type="tel"]');
+
+if (telInputs) {
+  telInputs.forEach(function (input) {
+    Inputmask('+7 (999) 999-99-99').mask(input);
+  });
+}
+
+'use strict';
+
 (function () {
   var togglesList = document.querySelector('.controls__list');
   var toggles = document.querySelectorAll('.controls__btn');
